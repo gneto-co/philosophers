eb708280d17897fce093287aaf913a4057e39f1b
gneto-co <gneto-co@student.42.fr>
2024-04-16 11:24:17 +0100

    now args like 12a will not be acepeted

--- side_functions/ft_atoi.c
+++ side_functions/ft_atoi.c
@@ -6,25 +6,52 @@
 /*   By: gneto-co <gneto-co@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 11:11:06 by gneto-co          #+#    #+#             */
-/*   Updated: 2024/03/26 12:56:38 by gneto-co         ###   ########.fr       */
+/*   Updated: 2024/04/16 11:23:42 by gneto-co         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "side.h"
 
+static int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+static int	ft_isnumber(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i++]))
+			return (0);
+	}
+	return (1);
+}
+
+static void	divide(int *nbr, int *signal)
+{
+	*nbr = 0;
+	*signal = 1;
+}
+
 int	ft_atoi(const char *nptr)
 {
 	int long	nbr;
-	int			sinal;
+	int			signal;
 
-	nbr = 0;
-	sinal = 1;
+	if (!ft_isnumber(nptr))
+		return (0);
+	divide(&nbr, &signal);
 	while ((*nptr >= 9 && *nptr <= 13) || *nptr == 32)
 		nptr++;
 	if (*nptr == '-' || *nptr == '+')
 	{
 		if (*nptr == '-')
-			sinal = -1;
+			signal = -1;
 		nptr++;
 	}
 	while (*nptr >= '0' && *nptr <= '9')
@@ -33,9 +60,9 @@ int	ft_atoi(const char *nptr)
 		nbr += *nptr - '0';
 		nptr++;
 	}
-	if (nbr * sinal > INT_MAX || nbr * sinal < INT_MIN)
+	if (nbr * signal > INT_MAX || nbr * signal < INT_MIN)
 		nbr = 0;
 	if (nbr)
-		nbr *= sinal;
+		nbr *= signal;
 	return (nbr);
 }
